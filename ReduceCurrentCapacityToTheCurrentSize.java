d9c479b480a968d84c80a3fdf9b3e5adf7add3d3
Srija444 <127313392+Srija444@users.noreply.github.com>
2023-05-01 04:03:32 -0700

    Add files via upload

---
+++ ReduceCurrentCapacityToTheCurrentSize.java
@@ -0,0 +1,23 @@
+package practiceoncollections;
+import java.util.*;
+public class ReduceCurrentCapacityToTheCurrentSize {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		al.ensureCapacity(20);
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter first number: ");
+		int fnum=s.nextInt();
+		al.add(fnum);
+		System.out.println("enter second number: ");
+		int snum=s.nextInt();
+		al.add(snum);
+		al.trimToSize();
+		System.out.println("the values are: ");
+		for(int in: al) {
+			System.out.println(in);
+		}
+		
+	}
+
+}
